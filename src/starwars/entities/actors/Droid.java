86610016f58e84c717032838be0d7c32554e62b1
James Sammut <jasam3@student.monash.edu>
2017-05-02 17:47:44 +1000

    Added a Droid SWActor Class
    Droid SWActor class that will implement the Droid characters to be used by the game.

---
+++ src/starwars/entities/actors/Droid.java
@@ -0,0 +1,88 @@
+package starwars.entities.actors;
+
+import java.util.ArrayList;
+
+import edu.monash.fit2099.gridworld.Grid;
+import edu.monash.fit2099.simulator.space.Direction;
+import edu.monash.fit2099.simulator.userInterface.MessageRenderer;
+import starwars.SWActor;
+import starwars.SWLocation;
+import starwars.SWWorld;
+import starwars.Team;
+import starwars.actions.Move;
+import starwars.entities.actors.behaviors.AttackInformation;
+import starwars.entities.actors.behaviors.AttackNeighbours;
+
+public class Droid extends SWActor {
+
+	private String name;
+
+	/**
+	 * Create a Droid At present coding, Droids are of team DROID,
+	 * so their attempts to attack other Droids won't be effectual.
+	 * 
+	 * @param hitpoints
+	 *            the number of hit points of this Tusken Raider. If this
+	 *            decreases to below zero, the Raider will die.
+	 * @param name
+	 *            this raider's name. Used in displaying descriptions.
+	 * @param m
+	 *            <code>MessageRenderer</code> to display messages.
+	 * @param world
+	 *            the <code>SWWorld</code> world to which this
+	 *            <code>TuskenRaider</code> belongs to
+	 * 
+	 */
+	public Droid(int hitpoints, String name, MessageRenderer m, SWWorld world) {
+		super(Team.DROID, 50, m, world);
+		// TODO Auto-generated constructor stub
+		this.name = name;
+	}
+
+	@Override
+	public void act() {
+		if (isDead()) {
+			return;
+		}
+		say(describeLocation());
+
+		AttackInformation attack = AttackNeighbours.attackLocals(this, this.world, false, false);
+		if (attack != null) {
+			say(getShortDescription() + " has attacked" + attack.entity.getShortDescription());
+			scheduler.schedule(attack.affordance, this, 1);
+		}
+		else if (Math.random() > 0.5){
+			
+			ArrayList<Direction> possibledirections = new ArrayList<Direction>();
+
+			// build a list of available directions
+			for (Grid.CompassBearing d : Grid.CompassBearing.values()) {
+				if (SWWorld.getEntitymanager().seesExit(this, d)) {
+					possibledirections.add(d);
+				}
+			}
+
+			Direction heading = possibledirections.get((int) (Math.floor(Math.random() * possibledirections.size())));
+			say(getShortDescription() + "is heading " + heading + " next.");
+			Move myMove = new Move(heading, messageRenderer, world);
+
+			scheduler.schedule(myMove, this, 1);
+		}
+	}
+
+	@Override
+	public String getShortDescription() {
+		return name + " the Droid";
+	}
+
+	@Override
+	public String getLongDescription() {
+		return this.getShortDescription();
+	}
+
+	private String describeLocation() {
+		SWLocation location = this.world.getEntityManager().whereIs(this);
+		return this.getShortDescription() + " [" + this.getHitpoints() + "] is at " + location.getShortDescription();
+
+	}
+}
