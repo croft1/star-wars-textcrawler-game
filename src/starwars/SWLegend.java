b86ab7f4db3f9a41c11d713427400e84d66b9459
James Sammut <jasam3@student.monash.edu>
2017-05-11 21:54:59 +1000

    Additional classes receiving references
    Better to be thorough!

--- src/starwars/SWLegend.java
+++ src/starwars/SWLegend.java
@@ -61,3 +61,23 @@ public abstract class SWLegend extends SWForceActor {
 
 	protected abstract void legendAct();
 }
+
+/*
+REFERENCES
+
+Javatpoint 2017, Java Switch Statement, viewed 10 May 2017,
+https://www.javatpoint.com/java-switch 
+
+Stack Overflow 2011, Getting random numbers in Java [duplicate], viewed 10 May 2017,
+http://stackoverflow.com/questions/5887709/getting-random-numbers-in-java
+
+Stack Overflow 2010, In Java, how do I check if a string contains a substring (ignoring case)? [duplicate], viewed 7 May 2017,
+http://stackoverflow.com/questions/2275004/in-java-how-do-i-check-if-a-string-contains-a-substring-ignoring-case
+
+Stack Overflow 2010, In Java how does one turn a String into a char or a char into a String?, viewed 10 April 2017,
+http://stackoverflow.com/questions/2429228/in-java-how-does-one-turn-a-string-into-a-char-or-a-char-into-a-string
+
+The Internet Movie Database 2017, Quotes for C-3PO (Character), viewed 10 April 2017,
+http://www.imdb.com/character/ch0000048/quotes 
+
+*/
