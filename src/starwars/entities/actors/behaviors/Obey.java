9c9a11adbf2df00b365a3d8ddd4df12230053fc3
James Sammut <jasam3@student.monash.edu>
2017-05-26 12:10:32 +1000

    Some more changes for Javadoc comments

--- src/starwars/entities/actors/behaviors/Obey.java
+++ src/starwars/entities/actors/behaviors/Obey.java
@@ -7,17 +7,38 @@ import java.util.Collection;
 import edu.monash.fit2099.simulator.space.Direction;
 import starwars.swinterfaces.SWGridController;
 
+/**
+ * Class for Stormtroopers
+ * 
+ * A soldier of the Empire. Will protect Vader at all costs - will try to call for backup if needed
+ * also, which can be a problem for the player!
+ * 
+ * @author jas
+ * @author mewc
+ */
 public class Obey {
 
 	private ArrayList<Direction> moves;
 	private int position = 0;
 	
+	/**
+	 * Obey(moves) method for actor behaviour
+	 * 
+	 * Sets the list of available moves that this actor can do.
+	 * 
+	 * @param 	moves	- A Array of Directions that this actor can traverse.
+	 */
 	public Obey(Direction [] moves) {
 		this.moves = new ArrayList<Direction>(Arrays.asList(moves));
-		
-		// TODO Auto-generated constructor stub
+	
 	}
-
+	/**
+	 * Obey(moves) method for actor behaviour
+	 * 
+	 * Sets the list of available moves that this actor can do.
+	 * 
+	 * @param 	moves	- A Collection of Directions that this actor can traverse.
+	 */
 	public Obey(Collection<Direction> moves) {
 		this.moves = new ArrayList<Direction>(moves);
 	}
